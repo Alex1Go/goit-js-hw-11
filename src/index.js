478aea7502f87548585bdde2fb6aa02fd81f9bd3
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2023-07-13 20:54:06 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
-// 38225262-d1745fccc0f8e035d7be8e954
+import { PixabayAPI } from "./js/pixabay-api";
+import Notiflix from 'notiflix';
+
+const searchForm = document.querySelector('#search-form');
+const inputEl = searchForm.firstElementChild;
+const galleryEl = document.querySelector('.gallery');
+const btnMore = document.querySelector('.load-more')
+
+searchForm.addEventListener('submit', onSearchForm);
+
+const pixabayInctance = new PixabayAPI();
+
+function onSearchForm(evt) {
+    evt.preventDefault();
+    const searchQuery = inputEl.value.trim();
+    if (!searchQuery) {
+        return;
+    }
+    pixabayInctance.query = searchQuery;
+
+    pixabayInctance.getImages().then(data => {
+        if (!data.results.length) {
+            Notiflix.Report.failure("Sorry, there are no images matching your search query. Please try again.", '');
+            return; 
+        }
+       
+   })
+}
+
+function createMarkup(images) {
+  if (!gallery) {
+    return;
+  }
+  const markup = images.map(image => {
+      const {
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = image;
+      return `
+        <a class="gallery__link" href="${largeImageURL}">
+          <div class="gallery-item">
+            <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item"><b>Likes</b>${likes}</p>
+              <p class="info-item"><b>Views</b>${views}</p>
+              <p class="info-item"><b>Comments</b>${comments}</p>
+              <p class="info-item"><b>Downloads</b>${downloads}</p>
+            </div>
+          </div>
+        </a>
+      `;
+    })
+    .join('');
+
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+
+};
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,1 +1,20 @@
-import axios from "axios";
+ import axios from "axios";
+
+export class PixabayAPI {
+    #API_KEY = '38225262-d1745fccc0f8e035d7be8e954';
+    #BASE_URL = 'https://pixabay.com/api/';
+
+    query = null;
+    page = 1;
+
+    async getImages() {
+        return await axios.get('${this.#BASE_URL}?key=${this.#API_KEY}&q=${this.query}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40')
+            // .then((response) => { 
+            //     if (!response.ok) {
+            //         throw new Error(response.status);
+            //     }
+            //     return response.json();
+            //  })
+    }
+}
+
