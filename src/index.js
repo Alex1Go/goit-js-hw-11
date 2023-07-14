f0c7e09245bf1e69fc09ae183c33a365aafe0300
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2023-07-14 18:07:12 +0300

    add

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const btnMore = document.querySelector('.load-more')
 const pixabayInctance = new PixabayAPI();
 
 function createMarkup(images) {
-  if (!gallery) {
+  if (!galleryEl) {
     return;
   }
   const markup = images.map(image => {
@@ -50,17 +50,16 @@ const onSearchForm = async evt => {
     pixabayInctance.page = 1;
     try {
         const { data } = await pixabayInctance.getImages();
-        if (!data.results.length) {
+        if (!data.hits.length) {
             Notiflix.Report.failure("Sorry, there are no images matching your search query. Please try again.", '');
             throw new Error();
         }
-        galleryEl.innerHTML = createMarkup()
+        galleryEl.innerHTML = createMarkup(data.hits)
     } catch (err) {
         console.log(err.message);
     }
      
 };
-
 
 searchForm.addEventListener('submit', onSearchForm);
 
