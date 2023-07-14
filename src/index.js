8c9a09f7d03e5bcccb24a84c9e55db0134437bce
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2023-07-14 14:55:44 +0300

    add

--- src/index.js
+++ src/index.js
@@ -8,25 +8,6 @@ const btnMore = document.querySelector('.load-more')
 
 const pixabayInctance = new PixabayAPI();
 
-const onSearchForm = async evt => {
-    evt.preventDefault();
-
-    const searchQuery = inputEl.value.trim();
-    pixabayInctance.query = searchQuery;
-    pixabayInctance.page = 1;
-    try {
-        const { data } = await pixabayInctance.getImages();
-        if (!data.results.length) {
-            Notiflix.Report.failure("Sorry, there are no images matching your search query. Please try again.", '');
-            throw new Error();
-        }
-        galleryEl.innerHTML = createMarkup()
-    } catch (err) {
-        console.log(err.message);
-    }
-     
-};
-
 function createMarkup(images) {
   if (!gallery) {
     return;
@@ -59,5 +40,26 @@ function createMarkup(images) {
 
   galleryEl.insertAdjacentHTML('beforeend', markup);
 
- };
+};
+ 
+const onSearchForm = async evt => {
+    evt.preventDefault();
+
+    const searchQuery = inputEl.value.trim();
+    pixabayInctance.query = searchQuery;
+    pixabayInctance.page = 1;
+    try {
+        const { data } = await pixabayInctance.getImages();
+        if (!data.results.length) {
+            Notiflix.Report.failure("Sorry, there are no images matching your search query. Please try again.", '');
+            throw new Error();
+        }
+        galleryEl.innerHTML = createMarkup()
+    } catch (err) {
+        console.log(err.message);
+    }
+     
+};
+
+
 searchForm.addEventListener('submit', onSearchForm);
